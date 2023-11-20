55e1ec719b81e9702941c8b0c29c60cdf7fd412c
nytouu <capitaine7312@gmail.com>
2023-11-20 12:38:09 +0100

    check if crepe close to pan

--- Assets/Scripts/PanMovement.cs
+++ Assets/Scripts/PanMovement.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class PanMovement : MonoBehaviour
 {
-    public float threshhold = 3f;
+    public float forceThreshold = 3f;
+    public float distanceThreshold = 2.5f;
     public Krampou krampouez;
 
     private Rigidbody rb;
-    private bool colliding;
 
     // Start is called before the first frame update
     void Start()
     {
-        colliding = false;
         rb = GetComponent<Rigidbody>();
         Input.compensateSensors = true;
         Input.gyro.enabled = true;
@@ -31,11 +30,11 @@ public class PanMovement : MonoBehaviour
 
         rb.MoveRotation(Quaternion.Euler(rotation));
 
-        if (Input.gyro.userAcceleration.z > threshhold) {
+        /* Debug.Log(Vector3.Distance(transform.position, krampouez.transform.position)); */
+
+        if (Input.gyro.userAcceleration.z >= forceThreshold &&
+                Vector3.Distance(transform.position, krampouez.transform.position) <= distanceThreshold) {
             krampouez.JumpFlip();
-            /* if (colliding) { */
-                /* Debug.Log("crêpe turn"); */
-            /* } */
         }
     }
 
@@ -46,18 +45,5 @@ public class PanMovement : MonoBehaviour
         vector.y = tempZ;
         
         return vector;
-    }
-
-    private void OnCollisionEnter(Collision collision) {
-        if (krampouez == collision.gameObject.GetComponent<Krampou>()) {
-            colliding = true;
-            Debug.Log("colliding");
-        }
-    }
-    private void OnCollisionExit(Collision collision) {
-        if (krampouez == collision.gameObject.GetComponent<Krampou>()) {
-            colliding = false;
-            Debug.Log("not colliding");
-        }
     }
 }
