f483ac10d9ddd4879fad9c826469edad6385014a
nytouu <capitaine7312@gmail.com>
2023-11-27 15:17:57 +0100

    update scripts

--- Assets/Scripts/ClickableItem.cs
+++ Assets/Scripts/ClickableItem.cs
@@ -2,17 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickableItem : MonoBehaviour
+public class ClickableItem : MonoBehaviour, ITouchable
 {
+    private bool selected;
+
+    private Vector3 defaultPosition;
+
+    private MeshRenderer currentMesh;
+
+    private Vector3 selectedPosition;
+
+    public void OnTouchedDown(Vector3 touchPosition)
+    {
+        if (!selected) {
+            currentMesh.material.color = Color.red;
+        }
+    }
+
+    public void OnTouchedStay(Vector3 touchPosition)
+    {
+        if (!selected) {
+            transform.position = touchPosition;
+        }
+    }
+
+    public void OnTouchedUp()
+    {
+        if (!selected) {
+            transform.position = defaultPosition;
+            currentMesh.material.color = Color.white;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        selected = false;
+
+        defaultPosition = transform.position;
+        currentMesh = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        selected = true;
+        transform.position = selectedPosition;
+        Debug.Log("touch");
     }
 }
