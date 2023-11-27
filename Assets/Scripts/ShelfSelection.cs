a977008d95b1e14840bbae738fc5272e494e5421
nytouu <capitaine7312@gmail.com>
2023-11-27 09:45:41 +0100

    import touch plugin

--- Assets/Scripts/ShelfSelection.cs
+++ Assets/Scripts/ShelfSelection.cs
@@ -2,8 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShelfSelection : MonoBehaviour
+public class ShelfSelection : MonoBehaviour, ITouchable
 {
+    public void OnTouchedDown(Vector3 touchPosition)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnTouchedStay(Vector3 touchPosition)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnTouchedUp()
+    {
+        throw new System.NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
