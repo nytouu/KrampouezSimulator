1a3837ec238ddccc8616e2c0cd7885422ee07966
nytouu <capitaine7312@gmail.com>
2023-12-04 10:14:36 +0100

    mix ingredient by shaking phone

--- Assets/Scripts/MixStick.cs
+++ Assets/Scripts/MixStick.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class MixStick : MonoBehaviour
 {
+	private float counter;
+
+	public float speed = 1f;
+	public float diameter = 1f;
+	public float threshold = 0.5f;
+
+	private Vector3 defaultPosition;
+	private float shakePower;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		defaultPosition = transform.position;
+		counter = 0;
+		shakePower = 0;
+
+        Input.compensateSensors = true;
+        Input.gyro.enabled = true;
+
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
     }
 
     // Update is called once per frame
     void Update()
     {
+		Vector3 acceleration = Input.gyro.userAcceleration;
+
+		if (acceleration.x > threshold || acceleration.y > threshold || acceleration.z > threshold){
+			shakePower = Mathf.Abs(acceleration.x) + Mathf.Abs(acceleration.y) + Mathf.Abs(acceleration.z);
+		} else {
+			shakePower = 0;
+		}
+
+		counter += Time.deltaTime * shakePower * speed;
         
+		float x = Mathf.Cos(counter) * diameter + defaultPosition.x;
+		float y = transform.position.y;
+		float z = Mathf.Sin(counter) * diameter + defaultPosition.z;
+
+		transform.position = new Vector3(x, y, z);
     }
 }
