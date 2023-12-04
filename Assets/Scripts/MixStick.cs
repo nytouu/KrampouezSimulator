8345b0bbd6ac07ce67378cdf5ecd093447d39ea8
nytouu <capitaine7312@gmail.com>
2023-12-04 14:29:27 +0100

    add progress

--- Assets/Scripts/MixStick.cs
+++ Assets/Scripts/MixStick.cs
@@ -12,6 +12,7 @@ public class MixStick : MonoBehaviour
 
 	private Vector3 defaultPosition;
 	private float shakePower;
+	private float progress;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,7 @@ public class MixStick : MonoBehaviour
 		defaultPosition = transform.position;
 		counter = 0;
 		shakePower = 0;
+		progress = 0;
 
         Input.compensateSensors = true;
         Input.gyro.enabled = true;
@@ -33,12 +35,13 @@ public class MixStick : MonoBehaviour
 
 		if (acceleration.x > threshold || acceleration.y > threshold || acceleration.z > threshold){
 			shakePower = Mathf.Abs(acceleration.x) + Mathf.Abs(acceleration.y) + Mathf.Abs(acceleration.z);
+			progress += shakePower;
 		} else {
 			shakePower = 0;
 		}
 
 		counter += Time.deltaTime * shakePower * speed;
-        
+ 
 		float x = Mathf.Cos(counter) * diameter + defaultPosition.x;
 		float y = transform.position.y;
 		float z = Mathf.Sin(counter) * diameter + defaultPosition.z;
