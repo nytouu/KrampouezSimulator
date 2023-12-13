ada5541be5935d581c99f820fbe112c7f14d67c2
nytouu <capitaine7312@gmail.com>
2023-12-13 11:33:00 +0100

    progressive cooking

--- Assets/Scripts/Krampou.cs
+++ Assets/Scripts/Krampou.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
 
+public enum State {
+	Uncooked,
+	Cooked,
+	TooCooked,
+	Cramed
+}
 public class Krampou : MonoBehaviour
 {
 	public float force = 300f;
 	public float duration;
 
 	public Color colorStart;
-	public Color colorEnd;
+	public Color colorCooked;
+	public Color colorTooCooked;
+	public Color colorCramed;
 
 	public PanMovement pan;
     private Rigidbody rb;
 	private MeshRenderer mesh;
 
-	private bool failed;
+	private State state;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +28,7 @@ public class Krampou : MonoBehaviour
 		rb = GetComponent<Rigidbody>();
 		mesh = GetComponent<MeshRenderer>();
 
-		failed = false;
+		state = State.Uncooked;
 
 		transform.position = new Vector3(pan.transform.position.x, pan.transform.position.y + 20, pan.transform.position.z);
     }
@@ -37,13 +45,26 @@ public class Krampou : MonoBehaviour
 	}
 
 	public void Cook(){
-		if (!failed) {
-			mesh.material.color = Color.Lerp(colorStart, colorEnd, Time.time / duration);
+		switch (state) {
+			case State.Uncooked:
+				ChangeColor(colorStart, colorCooked, State.Cooked);
+				break;
+			case State.Cooked:
+				ChangeColor(colorCooked, colorTooCooked, State.TooCooked);
+				break;
+			case State.TooCooked:
+				ChangeColor(colorTooCooked, colorCramed, State.Cramed);
+				break;
+			case State.Cramed:
+				break;
+		}
+	}
 
-			if (mesh.material.color == colorEnd) {
-				failed = true;
-				Debug.Log("Cramed");
-			}
+	private void ChangeColor(Color start, Color target, State next) {
+		mesh.material.color = Color.Lerp(colorStart, target, Time.time / duration);
+		if (mesh.material.color == target) {
+			state = next;
+			Debug.Log(state);
 		}
 	}
 }
