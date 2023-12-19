f6af3b03804285b071429705e2cd4abd78df5d2e
nytouu <capitaine7312@gmail.com>
2023-12-19 11:15:47 +0100

    materials

--- Assets/Scripts/SelectBox.cs
+++ Assets/Scripts/SelectBox.cs
@@ -25,7 +25,6 @@ public class SelectBox : MonoBehaviour, IMiniGame
 
 	public void AddIngredientToList(GameObject ingredient) {
 		selectedList.Add(ingredient);
-		Debug.Log(selectedList.Count);
 
 		if (selectedList.Count == 3){
 			gameManager.NextGame();
